28c17596f08436d13273cd116ef628c92209ba74
Lorenzo Cinque <lore.cinque@gmail.com>
2023-05-21 15:12:24 +0100

    Add PGConnection in the happy test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,7 @@
 use std::net::TcpListener;
 
-use zero2prod::startup::run;
+use sqlx::{Connection, PgConnection};
+use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::test]
 async fn health_check_works() {
@@ -24,6 +25,11 @@ async fn health_check_works() {
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres");
     let client = reqwest::Client::new();
 
     // Act
